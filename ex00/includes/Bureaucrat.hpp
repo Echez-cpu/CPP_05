98843700a39b7f706ada7b58df1f03fdb8f4f262
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-05 14:59:09 +0100

    Create Bureaucrat.hpp

---
+++ ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <climits>
+
+typedef std::string str;
+
+class Bureaucrat {
+	public:
+		Bureaucrat(str name, int grade);
+		Bureaucrat();
+		Bureaucrat(Bureaucrat const &source);
+		~Bureaucrat();
+
+		Bureaucrat &	operator=(Bureaucrat const &original_copy);
+		str				getName() const;
+		int				getGrade() const;
+
+		void			promote();
+		void			demote();
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char*	what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char*	what() const throw();
+		};
+	private:
+		str const	input_name;
+		int			input_grade;
+};
+
+std::ostream &	operator<<(std::ostream& output, Bureaucrat const &tobeFilled);
+
+#endif
