ecdbd8e79fa92b4956ffaefb70ae835c7da8d7ba
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-07 17:02:39 +0100

    Create ShrubberyCreationForm.hpp

---
+++ ex02/includes/ShrubberyCreationForm.hpp
@@ -0,0 +1,28 @@
+#ifndef  SHRUBBERYCREATIONFORM_HPP
+#define  SHRUBBERYCREATIONFORM_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "AForm.hpp"
+
+typedef std::string str;
+
+class ShrubberyCreationForm : public AForm {
+	public:
+		ShrubberyCreationForm(str target);
+		ShrubberyCreationForm(ShrubberyCreationForm const &src);
+		~ShrubberyCreationForm();
+
+		ShrubberyCreationForm &	operator=(ShrubberyCreationForm const &rSym);
+
+		void	beSigned(Bureaucrat & person);
+		void	execute(Bureaucrat & executor) const;
+	private:
+		str	input_target;
+};
+
+std::ostream &	operator<<(std::ostream & output, ShrubberyCreationForm const &shru);
+
+#endif
