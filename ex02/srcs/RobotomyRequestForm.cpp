b95195ede18df1f0aef23b6df91458d49f7c842b
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-07 22:50:48 +0100

    Update RobotomyRequestForm.cpp

--- ex02/srcs/RobotomyRequestForm.cpp
+++ ex02/srcs/RobotomyRequestForm.cpp
@@ -6,45 +6,43 @@ RobotomyRequestForm::RobotomyRequestForm(str target) : AForm("Robocop", 72, 45),
 
 RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &source) : AForm::AForm(source), input_target(source.input_target) {
 	std::cout << "[C] RobotomyRequestForm copy constructor called." << std::endl;
-	*this = src;
+	*this = source;
 }
 
 RobotomyRequestForm::~RobotomyRequestForm() {
-	std::cout << "[D] RobotomyRequestForm default destructor called." << std::endl;
+	std::cout << "[Destructor] RobotomyRequestForm default destructor called." << std::endl;
 }
 
-RobotomyRequestForm &	RobotomyRequestForm::operator=(RobotomyRequestForm const &rSym) {
-	if (this != &rSym) {
-		this->_target = rSym._target;
+RobotomyRequestForm &	RobotomyRequestForm::operator=(RobotomyRequestForm const &original_copy) {
+	if (this != &original_copy) {
+		this->input_target = original_copy.input_target;
 	}
 	return *this;
 }
 
-std::ostream &	operator<<(std::ostream & o, RobotomyRequestForm const &rSym) {
-	o << "Form name : " << rSym.getName() << std::endl;
-	o << "Signed : ";
-	if (rSym.getSigned())
-		o << "True" << std::endl;
+std::ostream &	operator<<(std::ostream & o, RobotomyRequestForm const &robo) {
+	output << "Form name : " << robo.getName() << std::endl;
+	output << "Signed : ";
+	if (robo.getSigned())
+		output << "True" << std::endl;
 	else
-		o << "False" << std::endl;
-	o << "Need to be grade " << rSym.getSignGrade() << " to sign it, and grade " << rSym.getExecuteGrade() << " to execute it.";
-	return o;
+		output << "False" << std::endl;
+	output << "Need to be grade " << robo.getSignGrade() << " to sign it, and grade " << robo.getExecuteGrade() << " to execute it.";
+	return output;
 }
 
 void	RobotomyRequestForm::execute(Bureaucrat & executor) const {
 	executor.executeForm(*this);
-	if (this->_signed == true) {
+	if (this->input_signed == true) {
 		std::cout << "*Some drilling noise*" << std::endl;
-		sleep(1);
 		std::cout << "*Some drilling noise*" << std::endl;
-		sleep(1);
 		std::cout << "*Some drilling noise*" << std::endl;
-		sleep(1);
-		srand(time(NULL));
+		
+		srand(time(NULL));   // call this in the main function
 		int	nb = rand() % 100 + 1;
 		if (nb % 2 == 0)
-			std::cout << this->_target.getName() << " has been robotomized." << std::endl;
+			std::cout << this->input_target << " has been robotomized." << std::endl;
 		else
-			std::cout << this->_target.getName() << " was not robotomized." << std::endl;
+			std::cout << this->input_target << " failed." << std::endl;
 	}
 }
