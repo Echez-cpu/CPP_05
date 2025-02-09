2261d089c6d6de306f906793214bca3036b3d098
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-09 11:41:48 +0100

    Create RobotomyRequestForm.cpp

---
+++ ex03/srcs/RobotomyRequestForm.cpp
@@ -0,0 +1,53 @@
+#include "../includes/RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(str target) : AForm("Robotomy request", 72, 45), input_target(target) {
+	std::cout << "[Constructor] RobotomyRequestForm default constructor called." << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &source) : AForm::AForm(source), input_target(source.input_target) {
+	std::cout << "[C] RobotomyRequestForm copy constructor called." << std::endl;
+	*this = source;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {
+	std::cout << "[Destructor] RobotomyRequestForm default destructor called." << std::endl;
+}
+
+RobotomyRequestForm &	RobotomyRequestForm::operator=(RobotomyRequestForm const &original_copy) {
+	if (this != &original_copy) {
+	AForm::operator=(original_copy);		
+	this->input_target = original_copy.input_target;
+	}
+	return *this;
+}
+
+std::ostream &	operator<<(std::ostream & output, RobotomyRequestForm const &robo) {
+	output << "Form name : " << robo.getName() << std::endl;
+	output << "Signed : ";
+	if (robo.getSigned())
+		output << "True" << std::endl;
+	else
+		output << "False" << std::endl;
+	output << "Need to be grade " << robo.getSignGrade() << " to sign it, and grade " << robo.getExecuteGrade() << " to execute it.";
+	return output;
+}
+
+void	RobotomyRequestForm::execute(Bureaucrat & executor) const {
+	executor.executeForm(*this);
+	if (getSigned() == true) {
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		std::cout << "*Some drilling noise*" << std::endl;
+		
+		srand(time(NULL));   // call this in the main function
+		int	nb = rand() % 100 + 1;
+		if (nb % 2 == 0)
+			std::cout << this->input_target << " has been robotomized." << std::endl;
+		else
+			std::cout << this->input_target << " failed, so didn't robotomized." << std::endl;
+	}
+}
